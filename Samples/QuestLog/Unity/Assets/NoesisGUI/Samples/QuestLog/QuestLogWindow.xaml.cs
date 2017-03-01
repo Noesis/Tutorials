88650f0ee5bdab7f35009300e9c3e82a6fe4c891
sfernandez <sfernandez@noesisengine.com>
2017-02-28 21:46:15 -0800

    Added QuestLog sample from NoesisGUI 2.0 Unity package.

---
+++ Samples/QuestLog/Unity/Assets/NoesisGUI/Samples/QuestLog/QuestLogWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+#endif
+
+#if NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace Noesis.Samples
+{
+    /// <summary>
+    /// Interaction logic for QuestLogWindow.xaml
+    /// </summary>
+    public partial class QuestLogWindow : UserControl
+    {
+        public QuestLogWindow()
+        {
+            this.Initialized += OnInitialized;
+            this.InitializeComponent();
+        }
+
+#if NOESIS
+        void InitializeComponent()
+        {
+            Noesis.GUI.LoadComponent(this, "Assets/NoesisGUI/Samples/QuestLog/QuestLogWindow.xaml");
+        }
+#endif
+
+        private void OnInitialized(object sender, EventArgs args)
+        {
+            this.DataContext = new QuestLogViewModel((ResourceDictionary)Resources.MergedDictionaries[0]);
+        }
+    }
+}
