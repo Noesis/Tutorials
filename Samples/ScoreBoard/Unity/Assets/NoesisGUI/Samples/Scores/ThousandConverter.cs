4f7a1f49f9bcc93a372a54bbe8e4ad9801c528ba
sfernandez <sfernandez@noesisengine.com>
2017-02-28 21:51:42 -0800

    Added ScoreBoard sample from NoesisGUI 2.0 Unity package.

---
+++ Samples/ScoreBoard/Unity/Assets/NoesisGUI/Samples/Scores/ThousandConverter.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+#endif
+
+#if NOESIS
+using Noesis;
+#else
+using System.Windows.Data;
+#endif
+using System;
+using System.Globalization;
+
+namespace Noesis.Samples
+{
+	public class ThousandConverter: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return string.Format("{0:F2} K", (float)((int)value / 1000.0f));
+		}
+		
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
