3d9fbc30153f8b8d046ce8ca49d42eec6619be14
sfernandez <sfernandez@noesisengine.com>
2017-02-28 21:41:55 -0800

    Added Buttons sample from NoesisGUI 2.0 Unity package.

---
+++ Samples/Buttons/Unity/Assets/NoesisGUI/Samples/Buttons/Buttons.xaml.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+#endif
+
+#if NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace Noesis.Samples
+{
+    /// <summary>
+    /// Interaction logic for Buttons.xaml
+    /// </summary>
+    public partial class Buttons : UserControl
+    {
+        public Buttons()
+        {
+            this.Initialized += OnInitialized;
+            this.InitializeComponent();
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            Noesis.GUI.LoadComponent(this, "Assets/NoesisGUI/Samples/Buttons/Buttons.xaml");
+        }
+#endif
+
+        private void OnInitialized(object sender, EventArgs args)
+        {
+            this.DataContext = new ButtonsViewModel();
+        }
+    }
+}
---
+++ Samples/Buttons/Unity/Assets/NoesisGUI/Samples/Buttons/ButtonsViewModel.cs
@@ -0,0 +1,51 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+#endif
+
+#if NOESIS
+using Noesis;
+using UnityEngine;
+#else
+using System;
+#endif
+
+namespace Noesis.Samples
+{
+	/// <summary>
+	/// Buttons sample view model
+	/// </summary>
+	public class ButtonsViewModel
+	{
+		public ButtonsViewModel()
+		{
+			StartCommand = new DelegateCommand(this.Start);
+			SettingsCommand = new DelegateCommand(this.Settings);
+			ExitCommand = new DelegateCommand(this.Exit);
+		}
+		
+		public DelegateCommand StartCommand { get; private set; }
+		public DelegateCommand SettingsCommand { get; private set; }
+		public DelegateCommand ExitCommand { get; private set; }
+		
+		private void Start(object parameter)
+		{
+			#if NOESIS
+			Debug.Log("Start Game");
+			#endif
+		}
+		
+		private void Settings(object parameter)
+		{
+			#if NOESIS
+			Debug.Log("Change Settings");
+			#endif
+		}
+		
+		private void Exit(object parameter)
+		{
+			#if NOESIS
+			Debug.Log("Exit Game");
+			#endif
+		}
+	}
+}
