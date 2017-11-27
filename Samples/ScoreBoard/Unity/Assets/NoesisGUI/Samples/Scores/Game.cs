9579493b4c9b770b801b74bd700746c4c695c72c
Sergio Fernández <sfernandez@noesisengine.com>
2017-11-27 23:16:01 +0100

    ScoreBoard sample reviewed

--- Samples/ScoreBoard/Unity/Assets/NoesisGUI/Samples/Scores/Game.cs
+++ Samples/ScoreBoard/Unity/Assets/NoesisGUI/Samples/Scores/Game.cs
@@ -4,50 +4,35 @@ using System.Linq;
 
 namespace Noesis.Samples
 {
-	public class Game : System.ComponentModel.INotifyPropertyChanged
-    {		
-		public Game()
-		{
-			Players = new List<Player>();
-		}
-		
-		public string Name { get; set; }
-		public int ElapsedTime { get; set; }
-		public List<Player> Players { get; private set; }
-	
-		public int AllianceScore { get { return Players.Where(p => p.Team == Team.Alliance).Sum(p => p.Score); } }
-		public int HordeScore { get { return Players.Where(p => p.Team == Team.Horde).Sum(p => p.Score); } }
-		
-		private int _selectedTeam;
-		public int SelectedTeam
-		{
-			get { return _selectedTeam; }
-			set
-			{
-				if (_selectedTeam != value)
-				{
-					_selectedTeam = value;
-					OnPropertyChanged("SelectedTeam");
-					
-					#if UNITY
-					UnityEngine.Debug.Log("Selected team: " + _visibleTeams[_selectedTeam].ToString());
-					#endif
-				}
-			}
-		}
-		
-		private string[] _visibleTeams = new string[] { "Overall", "Alliance", "Horde" };
-		public string[] VisibleTeams { get { return _visibleTeams; } }
-
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string name)
+    public class Game : NotifyPropertyChangedBase
+    {        
+        public Game()
         {
-            System.ComponentModel.PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
+            Players = new List<Player>();
+        }
+        
+        public string Name { get; set; }
+        public int ElapsedTime { get; set; }
+        public List<Player> Players { get; internal set; }
+    
+        public int AllianceScore { get { return Players.Where(p => p.Team == Team.Alliance).Sum(p => p.Score); } }
+        public int HordeScore { get { return Players.Where(p => p.Team == Team.Horde).Sum(p => p.Score); } }
+        
+        private int _selectedTeam;
+        public int SelectedTeam
+        {
+            get { return _selectedTeam; }
+            set
             {
-                handler(this, new System.ComponentModel.PropertyChangedEventArgs(name));
+                if (_selectedTeam != value)
+                {
+                    _selectedTeam = value;
+                    OnPropertyChanged("SelectedTeam");
+                }
             }
         }
-	}
+        
+        private string[] _visibleTeams = new string[] { "Overall", "Alliance", "Horde" };
+        public string[] VisibleTeams { get { return _visibleTeams; } }
+    }
 }
