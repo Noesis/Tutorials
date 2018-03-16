73dace571336d5bcc888a66ed1ca6170bbf47bd8
Sergio Fernández <sfernandez@noesisengine.com>
2018-03-16 17:31:41 +0100

    Menu3D scene moved to Samples root as other demos. Removed old Common folder in QuestLog sample. Added NoesisTheme to Scoreboard Blend project so it looks the same as Unity.

--- Samples/Scoreboard/Unity/App.xaml.cs
+++ Samples/Scoreboard/Unity/App.xaml.cs
@@ -13,5 +13,11 @@ namespace SamplesWPF
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // NoesisTheme added to Application.Resources also so it runs exactly the same as inside Noesis
+            ResourceDictionary theme = (ResourceDictionary)LoadComponent(new Uri("NoesisTheme.xaml", UriKind.Relative));
+            Resources.MergedDictionaries.Insert(0, theme);
+        }
     }
 }
