8729b8ecb64cbf1714982f78eab129b9b1df50a9
Sergio Fernández <sfernandez@noesisengine.com>
2019-03-06 13:47:12 +0100

    Modified HelloWorld C# sample to use NuGet packages.

--- Samples/HelloWorld/C#/Src/App.xaml.cs
+++ Samples/HelloWorld/C#/Src/App.xaml.cs
@@ -12,16 +12,5 @@ namespace HelloWorld
     /// </summary>
     public partial class App : Application
     {
-#if NOESIS
-        protected override XamlProvider GetXamlProvider()
-        {
-            EmbeddedXaml[] xamls =
-            {
-                new EmbeddedXaml { filename = "App.xaml", resource = "App" },
-                new EmbeddedXaml { filename= "MainWindow.xaml", resource = "MainWindow" }
-            };
-            return new EmbeddedXamlProvider(xamls, Properties.Resources.ResourceManager);
-        }
-#endif
     }
 }
