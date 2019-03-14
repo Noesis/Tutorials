c4629e9df3c60811d0f7ddb8ff025203ff9dc986
Sergio Fernández <sfernandez@noesisengine.com>
2019-03-15 00:03:10 +0100

    Added Inventory managed sample.

---
+++ Samples/Inventory/C#/Src/DragAdornerBehavior.cs
@@ -0,0 +1,55 @@
+﻿#if NOESIS
+using Noesis;
+using NoesisApp;
+#else
+using System;
+using System.Windows;
+using System.Windows.Interactivity;
+#endif
+
+namespace Inventory
+{
+    class DragAdornerBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.AllowDrop = true;
+            this.AssociatedObject.DragEnter += OnDragEnter;
+            this.AssociatedObject.DragOver += OnDragOver;
+            this.AssociatedObject.DragLeave += OnDragLeave;
+            this.AssociatedObject.Drop += OnDrop;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.AllowDrop = false;
+            this.AssociatedObject.DragEnter -= OnDragEnter;
+            this.AssociatedObject.DragLeave -= OnDragLeave;
+            this.AssociatedObject.Drop -= OnDrop;
+
+            base.OnDetaching();
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+        }
+
+        private void OnDragOver(object sender, DragEventArgs e)
+        {
+            Point position = e.GetPosition(this.AssociatedObject);
+            ViewModel.Instance.DraggedItemX = position.X;
+            ViewModel.Instance.DraggedItemY = position.Y;
+        }
+
+        private void OnDragLeave(object sender, DragEventArgs e)
+        {
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;
+        }
+    }
+}
---
+++ Samples/Inventory/C#/Src/DropItemBehavior.cs
@@ -0,0 +1,77 @@
+﻿#if NOESIS
+using Noesis;
+using NoesisApp;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+#endif
+
+namespace Inventory
+{
+    class DropItemBehavior : Behavior<ContentControl>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.AllowDrop = true;
+            this.AssociatedObject.PreviewDragEnter += OnDragEnter;
+            this.AssociatedObject.PreviewDragLeave += OnDragLeave;
+            this.AssociatedObject.PreviewDrop += OnDrop;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.AllowDrop = false;
+            this.AssociatedObject.PreviewDragEnter -= OnDragEnter;
+            this.AssociatedObject.PreviewDragLeave -= OnDragLeave;
+            this.AssociatedObject.PreviewDrop -= OnDrop;
+
+            base.OnDetaching();
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+            ContentControl control = (ContentControl)sender;
+            Slot slot = (Slot)control.Content;
+
+            slot.IsDragOver = true;
+
+            e.Handled = true;
+        }
+
+        private void OnDragLeave(object sender, DragEventArgs e)
+        {
+            ContentControl control = (ContentControl)sender;
+            Slot slot = (Slot)control.Content;
+
+            slot.IsDragOver = false;
+
+            e.Handled = true;
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            Slot sourceSlot = (Slot)e.Data.GetData(typeof(Slot));
+            Slot targetSlot = (Slot)this.AssociatedObject.Content;
+            targetSlot.IsDragOver = false;
+
+            if (targetSlot.IsDropAllowed)
+            {
+                // Move any item in target slot to the source slot
+                sourceSlot.Item = targetSlot.Item;
+
+                // Move dragged item to the target slot
+                targetSlot.Item = ViewModel.Instance.DraggedItem;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+
+            e.Handled = true;
+        }
+    }
+}
