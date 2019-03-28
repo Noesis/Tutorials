f4630d49d2953d6f97def62e13542ea9e88195a2
Sergio Fernández <sfernandez@noesisengine.com>
2019-03-28 10:49:38 +0100

    ActionBar unity sample updated to NoesisGUI 2.2 version.

--- Samples/ActionBar/Unity/Assets/NoesisGUI/Samples/ActionBar/ActionBarControl.xaml.cs
+++ Samples/ActionBar/Unity/Assets/NoesisGUI/Samples/ActionBar/ActionBarControl.xaml.cs
@@ -23,20 +23,9 @@ namespace ActionBar
             InitializeComponent();
         }
 
-#if NOESIS
-        private void InitializeComponent()
-        {
-            Noesis.GUI.LoadComponent(this, "Assets/NoesisGUI/Samples/ActionBar/ActionBarControl.xaml");
-        }
-#endif
-
         private void OnInitialized(object sender, EventArgs e)
         {
-            _canvas = (Canvas)FindName("Canvas");
-            _floating = (Border)FindName("Floating");
-
-            Panel spells = (Panel)FindName("Spells");
-            foreach (UIElement spell in spells.Children.Cast<UIElement>())
+            foreach (UIElement spell in this.Spells.Children.Cast<UIElement>())
             {
                 spell.MouseDown += OnSpellMouseDown;
             }
@@ -53,37 +42,37 @@ namespace ActionBar
             if (spell != null)
             {
                 // Initiate drag
-                _floating.Background = spell.Background;
-                _floating.BorderBrush = spell.BorderBrush;
-                _floating.Visibility = Visibility.Visible;
+                this.Floating.Background = spell.Background;
+                this.Floating.BorderBrush = spell.BorderBrush;
+                this.Floating.Visibility = Visibility.Visible;
     
                 _offset = e.GetPosition(spell);
-                Point canvasPos = e.GetPosition(_canvas);
-                Canvas.SetLeft(_floating, canvasPos.X - _offset.X);
-                Canvas.SetTop(_floating, canvasPos.Y - _offset.Y);
+                Point screenPos = e.GetPosition(this.Screen);
+                Canvas.SetLeft(this.Floating, screenPos.X - _offset.X);
+                Canvas.SetTop(this.Floating, screenPos.Y - _offset.Y);
     
-                if (_floating.CaptureMouse())
+                if (this.Floating.CaptureMouse())
                 {
-                    _floating.MouseMove += OnFloatingDrag;
-                    _floating.MouseUp += OnFloatingDrop;
+                    this.Floating.MouseMove += OnFloatingDrag;
+                    this.Floating.MouseUp += OnFloatingDrop;
                 }
             }
         }
         
         void OnFloatingDrag(object sender, MouseEventArgs e)
         {
-            Point canvasPos = e.GetPosition(_canvas);
-            Canvas.SetLeft(_floating, canvasPos.X - _offset.X);
-            Canvas.SetTop(_floating, canvasPos.Y - _offset.Y);
+            Point screenPos = e.GetPosition(this.Screen);
+            Canvas.SetLeft(this.Floating, screenPos.X - _offset.X);
+            Canvas.SetTop(this.Floating, screenPos.Y - _offset.Y);
         }
     
         void OnFloatingDrop(object sender, MouseButtonEventArgs e)
         {
-            _floating.ReleaseMouseCapture();
-            _floating.MouseMove -= OnFloatingDrag;
-            _floating.MouseUp -= OnFloatingDrop;
+            this.Floating.ReleaseMouseCapture();
+            this.Floating.MouseMove -= OnFloatingDrag;
+            this.Floating.MouseUp -= OnFloatingDrop;
     
-            _floating.Visibility = Visibility.Collapsed;
+            this.Floating.Visibility = Visibility.Collapsed;
     
             for (uint i = 0; i < NumSlots; ++i)
             {
@@ -94,17 +83,30 @@ namespace ActionBar
                 if (pos.X >= 0.0f && pos.X < size.Width &&
                     pos.Y >= 0.0f && pos.Y < size.Height)
                 {
-                    slot.Background = _floating.Background;
-                    slot.BorderBrush = _floating.BorderBrush;
+                    slot.Background = this.Floating.Background;
+                    slot.BorderBrush = this.Floating.BorderBrush;
                     break;
                 }
             }
         }
 
+#if NOESIS
+        private void InitializeComponent()
+        {
+            Noesis.GUI.LoadComponent(this, "Assets/NoesisGUI/Samples/ActionBar/ActionBarControl.xaml");
+
+            Screen = (Canvas)FindName("Screen");
+            Spells = (WrapPanel)FindName("Spells");
+            Floating = (Border)FindName("Floating");
+        }
+
+        Canvas Screen;
+        WrapPanel Spells;
+        Border Floating;
+#endif
+
         #region Private members
-        Canvas _canvas;
         Point _offset;
-        Border _floating;
 
         const int NumSlots = 6;
         Border[] _slots = new Border[NumSlots];
