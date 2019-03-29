4e08b4742c12117e2d4afb0187d450e089ad6644
Sergio Fernández <sfernandez@noesisengine.com>
2019-03-29 21:14:04 +0100

    QuestLog unity sample updated to NoesisGUI 2.2 version.

--- Samples/QuestLog/Unity/Assets/NoesisGUI/Samples/QuestLog/ViewModel.cs
+++ Samples/QuestLog/Unity/Assets/NoesisGUI/Samples/QuestLog/ViewModel.cs
@@ -2,12 +2,14 @@
 #define NOESIS
 using Noesis;
 using UnityEngine;
+using System;
 using System.Collections.ObjectModel;
 #else
 using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 #endif
 
 namespace QuestLog
@@ -30,17 +32,26 @@ namespace QuestLog
 
     public class ViewModel : NotifyPropertyChangedBase
     {
-        public ViewModel() { } // used in design time DataContext
-
-        public ViewModel(ResourceDictionary questImages)
+        public ViewModel()
         {
             Quests = new ObservableCollection<Quest>();
+
+#if NET_2_0_SUBSET
+            // Net2.0 does not support 'pack://application' syntax and we need it to display images at editor time in Blend
+            ImageSource image0 = new BitmapImage(new Uri("Assets/NoesisGUI/Samples/QuestLog/Images/Image0.png", UriKind.Relative));
+            ImageSource image1 = new BitmapImage(new Uri("Assets/NoesisGUI/Samples/QuestLog/Images/Image1.png", UriKind.Relative));
+            ImageSource image2 = new BitmapImage(new Uri("Assets/NoesisGUI/Samples/QuestLog/Images/Image2.png", UriKind.Relative));
+#else
+            ImageSource image0 = new BitmapImage(new Uri("pack://application:,,,/QuestLog;component/Assets/NoesisGUI/Samples/QuestLog/Images/Image0.png"));
+            ImageSource image1 = new BitmapImage(new Uri("pack://application:,,,/QuestLog;component/Assets/NoesisGUI/Samples/QuestLog/Images/Image1.png"));
+            ImageSource image2 = new BitmapImage(new Uri("pack://application:,,,/QuestLog;component/Assets/NoesisGUI/Samples/QuestLog/Images/Image2.png"));
+#endif
 
             // Should be filled by application
             Quests.Add(new Quest
                 {
                     Title = "Nature's Uprising",
-                    Image = questImages["Image0"] as ImageSource,
+                    Image = image0,
                     Difficulty = QuestDifficulty.Easy,
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPellentesque molestie dolor ac leo convallis, non fermentum felis lobortis. Vivamus id turpis nibh.\n\nSed feugiat massa dolor, commodo hendrerit lectus dapibus sit amet.",
                     Completed = false
@@ -48,7 +59,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Calming the Wake",
-                    Image = questImages["Image1"] as ImageSource,
+                    Image = image1,
                     Difficulty = QuestDifficulty.Normal,
                     Description = "Nullam volutpat felis eget lorem dictum sodales.\nNulla egestas porttitor ipsum ut tincidunt. Nullam varius justo quis mi pulvinar rutrum at a ligula.\n\nAenean efficitur dolor vel elit varius, sit amet convallis nulla rutrum.",
                     Completed = true
@@ -56,7 +67,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Tomen's Curiosity",
-                    Image = questImages["Image2"] as ImageSource,
+                    Image = image2,
                     Difficulty = QuestDifficulty.Easy,
                     Description = "Quisque nec tortor in tortor.\nVestibulum tincidunt porttitor sit amet justo. Vestibulum vehicula, dui et vehicula finibus, est nunc efficitur sem, in tempus dolor dolor accumsan enim.\n\nUt rhoncus et orci quis vehicula. Nam bibendum at augue sit amet iaculis.",
                     Completed = false
@@ -64,7 +75,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Forest in Dispair",
-                    Image = questImages["Image0"] as ImageSource,
+                    Image = image0,
                     Difficulty = QuestDifficulty.Normal,
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPellentesque molestie dolor ac leo convallis, non fermentum felis lobortis. Vivamus id turpis nibh.\n\nSed feugiat massa dolor, commodo hendrerit lectus dapibus sit amet.",
                     Completed = true
@@ -72,7 +83,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Retaliation",
-                    Image = questImages["Image1"] as ImageSource,
+                    Image = image1,
                     Difficulty = QuestDifficulty.Hard,
                     Description = "Nullam volutpat felis eget lorem dictum sodales.\nNulla egestas porttitor ipsum ut tincidunt. Nullam varius justo quis mi pulvinar rutrum at a ligula.\n\nAenean efficitur dolor vel elit varius, sit amet convallis nulla rutrum.",
                     Completed = true
@@ -80,7 +91,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Living Hearts",
-                    Image = questImages["Image2"] as ImageSource,
+                    Image = image2,
                     Difficulty = QuestDifficulty.Easy,
                     Description = "Quisque nec tortor in tortor.\nVestibulum tincidunt porttitor sit amet justo. Vestibulum vehicula, dui et vehicula finibus, est nunc efficitur sem, in tempus dolor dolor accumsan enim.\n\nUt rhoncus et orci quis vehicula. Nam bibendum at augue sit amet iaculis.",
                     Completed = false
@@ -88,7 +99,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Scout their Defenses",
-                    Image = questImages["Image0"] as ImageSource,
+                    Image = image0,
                     Difficulty = QuestDifficulty.Easy,
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPellentesque molestie dolor ac leo convallis, non fermentum felis lobortis. Vivamus id turpis nibh.\n\nSed feugiat massa dolor, commodo hendrerit lectus dapibus sit amet.",
                     Completed = false
@@ -96,7 +107,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Rescue the Leader",
-                    Image = questImages["Image1"] as ImageSource,
+                    Image = image1,
                     Difficulty = QuestDifficulty.Hard,
                     Description = "Nullam volutpat felis eget lorem dictum sodales.\nNulla egestas porttitor ipsum ut tincidunt. Nullam varius justo quis mi pulvinar rutrum at a ligula.\n\nAenean efficitur dolor vel elit varius, sit amet convallis nulla rutrum.",
                     Completed = true
@@ -104,7 +115,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Golden Oportunity",
-                    Image = questImages["Image2"] as ImageSource,
+                    Image = image2,
                     Difficulty = QuestDifficulty.Hard,
                     Description = "Quisque nec tortor in tortor.\nVestibulum tincidunt porttitor sit amet justo. Vestibulum vehicula, dui et vehicula finibus, est nunc efficitur sem, in tempus dolor dolor accumsan enim.\n\nUt rhoncus et orci quis vehicula. Nam bibendum at augue sit amet iaculis.",
                     Completed = false
@@ -112,7 +123,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Loyal Subordinates",
-                    Image = questImages["Image0"] as ImageSource,
+                    Image = image2,
                     Difficulty = QuestDifficulty.Easy,
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPellentesque molestie dolor ac leo convallis, non fermentum felis lobortis. Vivamus id turpis nibh.\n\nSed feugiat massa dolor, commodo hendrerit lectus dapibus sit amet.",
                     Completed = true
@@ -120,7 +131,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Legacy of the Past",
-                    Image = questImages["Image1"] as ImageSource,
+                    Image = image1,
                     Difficulty = QuestDifficulty.Normal,
                     Description = "Nullam volutpat felis eget lorem dictum sodales.\nNulla egestas porttitor ipsum ut tincidunt. Nullam varius justo quis mi pulvinar rutrum at a ligula.\n\nAenean efficitur dolor vel elit varius, sit amet convallis nulla rutrum.",
                     Completed = true
@@ -128,7 +139,7 @@ namespace QuestLog
             Quests.Add(new Quest
                 {
                     Title = "Purging the Dead",
-                    Image = questImages["Image2"] as ImageSource,
+                    Image = image2,
                     Difficulty = QuestDifficulty.Hard,
                     Description = "Quisque nec tortor in tortor.\nVestibulum tincidunt porttitor sit amet justo. Vestibulum vehicula, dui et vehicula finibus, est nunc efficitur sem, in tempus dolor dolor accumsan enim.\n\nUt rhoncus et orci quis vehicula. Nam bibendum at augue sit amet iaculis.",
                     Completed = false
