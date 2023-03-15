5e7a818e39b6187c083f237576f64659553ac5b0
s-fernandez-v <sfernandez@noesisengine.com>
2023-03-15 02:31:59 +0100

    Added Rive sample for Unity and Unreal

---
+++ Samples/Rive/UE5/Assets/Rive/Rive/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Samples/Rive/Unity/Assets/NoesisGUI/Samples/Rive/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using System;
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace Rive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            NoesisUnity.LoadComponent(this);
+        }
+#endif
+    }
+}
