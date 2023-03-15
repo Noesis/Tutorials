3ec288af4f5d7e924ad8556b33b3563dec0d9377
s-fernandez-v <sfernandez@noesisengine.com>
2023-03-15 10:47:48 +0100

    Added BackgroundBlur Unity sample

---
+++ Samples/BackgroundBlur/Unity/Assets/NoesisGUI/Samples/BackgroundBlur/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace BackgroundBlur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            NoesisUnity.LoadComponent(this);
+        }
+#endif
+    }
+}
